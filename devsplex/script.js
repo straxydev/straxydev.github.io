9fcfa9813502b02c9130ef2ca1f00670f9c73a85
straxydev <nikitachub007@gmail.com>
2025-07-10 12:37:16 +0300

    Update script.js

--- devsplex/script.js
+++ devsplex/script.js
@@ -25,3 +25,32 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const slideContainers = document.querySelectorAll('.slide-container');
+    
+    const observerOptions = {
+        threshold: 0.7, 
+        delay: 100,
+        rootMargin: '0px'
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            const overlay = entry.target.querySelector('.overlay');
+            if (entry.isIntersecting) {
+                if (overlay) {
+                    overlay.classList.add('visible');
+                }
+            } else {
+                
+                if (overlay) {
+                    overlay.classList.remove('visible');
+                }
+            }
+        });
+    }, observerOptions);
+
+    slideContainers.forEach(container => {
+        observer.observe(container);
+    });
+});
